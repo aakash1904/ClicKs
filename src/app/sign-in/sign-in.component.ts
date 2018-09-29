004a71edd82ad97d0fe1f160fe47fcc910b826a9
aakash <differaakash@gmail.com>
2018-09-30 03:11:13 +0530

    modified sign in enabled

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../user.model';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  check: boolean = true;
+
+  listofUsers: User[] = [
+      {email: 'differaakash@gmail.com', password: 'aakash'},
+      {email: 'rahulnadar@rediffmail.com', password: 'rahul'},
+      {email: 'tarandeep@gmail.com', password: 'tarandeep'}
+  ];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null, {validators: [Validators.email, Validators.required, Validators.minLength(10)]}),
+      'password': new FormControl(null, {validators: [Validators.required]}),
+    });
+  }
+
+  onSignin() {
+    this.listofUsers.forEach(item => {
+      if (item.email === this.form.value.email && item.password === this.form.value.password ) {
+        return this.router.navigate(['/home']);
+      } else {
+         this.check = false;
+         return this.form.reset();
+      }
+    });
+
+
   }
 
 }
