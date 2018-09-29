7be38123c8ccd838f232d53a0bfd09def2458c30
tarandeep2210 <rubal98@gmail.com>
2018-09-29 19:37:58 +0530

    added signin page and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { FormsModule } from '@angular/forms';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule,[ RouterModule.forRoot(
+
+      [
+        { path: 'signin', component: SignInComponent }
+      ]
+
+    ) ]
     
   ],
   providers: [],
