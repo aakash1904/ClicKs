bb77f0159e7f2831083914857dbc0e9958b1ab84
aakash <differaakash@gmail.com>
2018-10-06 15:04:01 +0530

    modified

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Clicks';
+
+  ngOnInit() {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyDnK0RN-ttEG00tt_b4Ufsz95lmTar8QGM",
+      authDomain: "ng-clicks.firebaseapp.com"
+    })
+
+  }
+}
